2ad936ff7c1643160ad7d7e978c049a639c5c27f
cooperll <cooperlindsey3927@gmail.com>
2022-07-13 14:56:01 -0700

    fixes for train/test/val split

--- yolov5/create_yolov5_dataset_yaml.py
+++ yolov5/create_yolov5_dataset_yaml.py
@@ -1,10 +1,19 @@
 import os
 
 
-def create_yolov5_dataset_yaml(yolo_train_dir: str, yolo_test_dir: str):
+def create_yolov5_dataset_yaml(
+    yolo_train_dir: str,
+    yolo_val_dir: str,
+    yolo_test_dir: str
+) -> None:
+    """
+    yolo_X_dir args should be relative to the yolov5 ultralytics
+    repo root, not relative to main.py
+    """
     yaml_file = "./yolov5/data/wider_face.yaml"
     train_images_dir = os.path.join(yolo_train_dir, "images")
-    val_images_dir = os.path.join(yolo_test_dir, "images")
+    val_images_dir = os.path.join(yolo_val_dir, "images")
+    test_images_dir = os.path.join(yolo_test_dir, "images")
 
     classes = ['Face']
     names_str = ""
@@ -15,5 +24,6 @@ def create_yolov5_dataset_yaml(yolo_train_dir: str, yolo_test_dir: str):
     with open(yaml_file, "w") as wobj:
         wobj.write("train: %s\n" % train_images_dir)
         wobj.write("val: %s\n" % val_images_dir)
+        wobj.write("test: %s\n" % test_images_dir)
         wobj.write("nc: %d\n" % len(classes))
         wobj.write(names_str + "\n")
--- yolov5/download_dataset.py
+++ yolov5/download_dataset.py
@@ -6,12 +6,12 @@ from datasets import load_dataset
 
 def download_dataset():
     wider_face_train = load_dataset('wider_face', split='train')
-    wider_face_test = load_dataset('wider_face', split='test')
     wider_face_val = load_dataset('wider_face', split='validation')
+    wider_face_test = load_dataset('wider_face', split='test')
 
     print("Num images in wider_face training set: %i" % (len(wider_face_train)))
-    print("Num images in wider_face test set: %i" % (len(wider_face_test)))
     print("Num images in wider_face val set: %i" % (len(wider_face_val)))
+    print("Num images in wider_face test set: %i" % (len(wider_face_test)))
 
     img = np.array(wider_face_train[110]['image'], dtype=np.uint8)
     faces = wider_face_train[110]['faces']
@@ -28,4 +28,4 @@ def download_dataset():
     # Uncomment to see an example of WiderFace, with bboxes drawn
     # plt.show()
 
-    return wider_face_train, wider_face_test, wider_face_val
+    return wider_face_train, wider_face_val, wider_face_test
